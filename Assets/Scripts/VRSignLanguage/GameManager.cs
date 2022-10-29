a40466ef740c99b58586084e3eac6011d5ccc438
agnieszkazielke <agnieszka.zielke@gmail.com>
2022-10-29 16:48:06 +0100

    UPDATE

--- Assets/Scripts/VRSignLanguage/GameManager.cs
+++ Assets/Scripts/VRSignLanguage/GameManager.cs
@@ -11,26 +11,15 @@ public class GameManager : MonoBehaviour
     [SerializeField] private List<GameObject> _signPosesRightHand = new List<GameObject>();
     [SerializeField] private HandShapeSkeletalDebugVisual _shapeHandDebug;
     [SerializeField] private TransformFeatureVectorDebugParentVisual _transformHandDebug;
-    [SerializeField] private CustomPoseRecognizer _poseChecker;
     [SerializeField] private Logger _debugLogger;
-    private int _signIndex;
+    private int _signIndex = -1;
     private GameObject _currentSign;
     
     void Start()
     {
-        SetFirstSign();
-        //_poseChecker.onPoseRecognised.AddListener(ConfigureSkeletonDebug);
-        
+        SelectNextSign();
     }
 
-    private void SetFirstSign()
-    {
-        _signIndex = 0;
-        _currentSign = _signPosesRightHand[_signIndex];
-        _currentSign.GetComponent<SelectorUnityEventWrapper>().WhenSelected.AddListener(DelaySelectNextSign);
-        ResetSkeletonDebug();
-        _debugLogger.LogInfo("Current sign: " + _currentSign.gameObject.name);
-    }
 
     private void DelaySelectNextSign()
     {
@@ -62,19 +51,12 @@ public class GameManager : MonoBehaviour
         _transformHandDebug.enabled = true;
         _shapeHandDebug._shapeRecognizerActiveState = _currentSign.GetComponent<ShapeRecognizerActiveState>();
         _transformHandDebug._transformRecognizerActiveState = _currentSign.GetComponent<TransformRecognizerActiveState>();
-        
-        
-        
-        //_poseChecker._transformRecognizerActiveStates.Clear();
-        //_poseChecker._transformRecognizerActiveStates.Add(_signPosesRightHand[_signIndex].GetComponent<TransformRecognizerActiveState>()); 
-         
-        //_poseChecker.ResetRecognizerStatus();
-
+  
     }
 
 
     private void OnDestroy()
     {
-        _poseChecker.onPoseRecognised.RemoveAllListeners();
+  
     }
 }
